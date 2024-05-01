34ece7bc1c483bd7a410bae7914f53782432c523
Charles La O <krloslao90@gmail.com>
2024-05-01 17:11:04 -0400

    Update crawler.js

--- crawler.js
+++ crawler.js
@@ -1,2 +1,26 @@
-import {load} from "cherrio";
+import {load} from "cheerio"
+
+async function crawlSite(baseURL, endpoint) {
+  const response = await fetch(baseURL + endpoint)
+  const html = await response.text()
+  const $ = load(html)
+
+  const discoveredHTMLAElements = $("a[href]")
+
+  const discoveredURLs = []
+  discoveredHTMLAElements.each((_, a) =>{
+    discoveredURLs.push($(a).attr("href"))
+  })
+
+  const filteredDiscoveredURLs = discoveredURLs.filter((url) => {
+    return (
+      url.startsWith(baseURL) &&
+      (!url.startsWith(`${baseURL}/wp-admin`) || url === `${baseURL}/wp-admin/admin-ajax.php`)
+    )
+  })
+
+  console.log(filteredDiscoveredURLs)
+}
+
+crawlSite("https://scrapeme.live/", "shop")
 
